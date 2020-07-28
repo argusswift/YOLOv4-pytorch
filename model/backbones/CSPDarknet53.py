60dc9b412868361ccda7b7636e1b01b4c15035bd
your name <your email>
2020-07-28 20:45:32 +0800

    Weight initialization

--- model/backbones/CSPDarknet53.py
+++ model/backbones/CSPDarknet53.py
@@ -1,6 +1,8 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 
 class Mish(nn.Module):
     def __init__(self):
@@ -118,6 +120,14 @@ class CSPDarknet53(nn.Module):
  
         self.feature_channels = feature_channels
         self.num_features = num_features
+        # Weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
 
     def forward(self, x):
         x = self.stem_conv(x)
